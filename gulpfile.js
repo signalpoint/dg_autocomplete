c7068932f511e051609f3dd43184aaa24a31081e
Tyler Frankenstein <tfrankenstein@crimewatchus.com>
2021-12-21 16:16:08 -0500

    Switch from grunt to gulp

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+var makeBinary = true;
+
+var gulp = require('gulp'),
+    watch = require('gulp-watch'),
+    gp_concat = require('gulp-concat'),
+    gp_rename = require('gulp-rename'),
+    gp_uglify = require('gulp-uglify'),
+    gp_minify_css = require('gulp-minify-css');
+
+var jsSrc = [
+  './src/_dg_autocomplete.js',
+  './src/includes/include.*.js',
+  './src/widgets/widget.*.js'
+];
+
+// Minify JavaScript
+function minifyJs() {
+  console.log('compressing dg_autocomplete.js...');
+  var js = gulp.src(jsSrc)
+    .pipe(gp_concat('dg_autocomplete.js'))
+    .pipe(gulp.dest('./'));
+  if (makeBinary) {
+    console.log('compressing dg_autocomplete.min.js...');
+    return js.pipe(gp_rename('dg_autocomplete.min.js'))
+    .pipe(gp_uglify())
+    .pipe(gulp.dest('./'));
+  }
+  return js;
+}
+gulp.task(minifyJs);
+
+gulp.task('default', function(done) {
+
+  gulp.watch(jsSrc, gulp.series('minifyJs'));
+
+  done();
+
+});
